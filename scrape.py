49d96698bcb8f605c4a11b31ef6033344de9d023
Yatharth <shah9yatharth@gmail.com>
2020-12-10 20:31:49 +0530

    Added the scraping script

---
+++ scrape.py
@@ -0,0 +1,24 @@
+import os
+import gzip
+import shutil
+import requests
+from bs4 import BeautifulSoup
+
+f_name = "name.basics.tsv"
+f_gz = f_name + ".gz"
+
+
+url = f"https://datasets.imdbws.com/{f_gz}"
+r = requests.get(url,allow_redirects=True)
+
+open(f_gz,'wb').write(r.content)
+
+with gzip.open(f_gz, 'rb') as f_in:
+    with open(f_in, 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+
+if os.path.exists(f_gz):
+  os.remove(f_gz)
+else:
+  print("The file does not exist")

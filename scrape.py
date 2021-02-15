b6ae3e5183cbb07535a41f53ed431d1c7ee8f6d4
Yatharth <shah9yatharth@gmail.com>
2021-02-15 22:36:17 +0530

    added other tsvs

--- scrape.py
+++ scrape.py
@@ -4,20 +4,31 @@ import shutil
 import requests
 from bs4 import BeautifulSoup
 
-f_name = "name.basics.tsv"
-f_gz = f_name + ".gz"
+artist_name = "name.basics.tsv"
+artist_name_gz = artist_name + ".gz"
 
+title_akas = "title.akas.tsv"
+title_akas_gz = title_akas + ".gz"
 
-url = f"https://datasets.imdbws.com/{f_gz}"
+title_basics = "title.basic.tsv"
+title_basics_gz = title_basics + ".gz"
+
+title_crew = "title.crew.tsv"
+title_crew_gz = title_crew + ".gz"
+
+title_ratings = "title.ratings.tsv"
+title_ratings_gz = title_ratings + ".gz"
+
+url = f"https://datasets.imdbws.com/{artist_name_gz}"
 r = requests.get(url,allow_redirects=True)
 
-open(f_gz,'wb').write(r.content)
+open(artist_name_gz,'wb').write(r.content)
 
-with gzip.open(f_gz, 'rb') as f_in:
-    with open(f_name, 'wb') as f_out:
+with gzip.open(artist_name_gz, 'rb') as f_in:
+    with open(artist_name, 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
 
-if os.path.exists(f_gz):
-  os.remove(f_gz)
+if os.path.exists(artist_name_gz):
+  os.remove(artist_name_gz)
 else:
   print("The file does not exist")

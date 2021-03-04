05f4b86b2e03ba5b6e6f237a252459eb1643c432
Yatharth <shah9yatharth@gmail.com>
2021-03-04 23:05:59 +0530

    changed telegram.py

--- alert/telegram.py
+++ alert/telegram.py
@@ -1,6 +1,15 @@
 from telegram.ext import Updater
 import os
+import logging
 
 bot_authtoken = os.environ.get("telegram_auth_token")
 
 updater = Updater(token=bot_authtoken)
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello World from Bot")

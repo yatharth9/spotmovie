05d04fbd7b4d03a2f272b2654effa75a2a25e62e
Anuj-negi2207 <66243709+Anuj-negi2207@users.noreply.github.com>
2021-09-10 20:39:12 +0530

    Updated DBwork.py (Better BackEnd)

--- internal/DBwork.py
+++ internal/DBwork.py
@@ -1,14 +1,10 @@
 #importing getposter to get posters
 import internal.Poster.getposter as gp
-
-"""#Use this While testing this particular py file
-import Poster.getposter as gp
-"""
+#import Poster.getposter as gp
 import mysql.connector
-import random
-import json
-
-#____________________________________________________________________________________________________________________
+import pandas as pd
+import time
+from tabulate import tabulate
 
 def TemplateCommand(type, value, end, String):
     #Here, 'type' = {where, AND, OR, JOIN, UNION, INTERSECTION}
@@ -20,48 +16,105 @@ def TemplateCommand(type, value, end, String):
 
 #____________________________________________________________________________________________________________________
 
-def GetFromOneGenre(MainOutputList, Movies, Cursor, year, random):
+def GetFromOneGenre(MainOutputList, Movies, Cursor, year, Range):
     for index in range(len(MainOutputList)):
         Gen = MainOutputList[index]    
         
-        command = "Select tconst, primaryTitle, genres, startYear from data2 where genres = '" + Gen + "';"
-
+        command = "Select tconst, primaryTitle, startYear, runtimeMinutes, genres, directors, averageRating, changeFactor, poster from details_data_movie where startYear > {0} ".format(year)
+        command = TemplateCommand('and ', Gen, ' ', command)
+        command += "and changeFactor<averageRating/100 ORDER BY changeFactor, averageRating DESC;"
         #print(command)
         Cursor.execute(command)
         Result = Cursor.fetchall()
-        TempMovies = []
         
+        """
+        max = 0
+        for j in Result:
+            if j[6]>max:
+                max = j[6]
+                value = j
+        """
+        """
+        max1 = 0
+        max2 = 9999999
+        value = []
+        for j in range(len(Result)):
+            if Result[j][6]>max1 and Result[j][7]<max2:
+                max1 = Result[j][6]
+                max2 = Result[j][7]
+                value.append(j)
+        """        
+        """
+        print("length - %d" %len(Result))
+        print("value - ")
+        print(value, end="\n\n")
+        """
+
+        for index in range(Range):
+            Movies.append(Result[index])
+        """
         for j in range(len(Result)):
             if int(Result[j][3])>year and Result[j][3] != '\\N':
                 TempMovies.append(Result[j])        
         
         j = random.randint(0, len(TempMovies)-1)
         MovieIndex = len(Movies)
+        print(MovieIndex)
         href_movie = f"https://www.imdb.com/title/{TempMovies[j][0]}/"
-        Poster = gp.getposter(tconst=str(TempMovies[j][0]), Name=str(TempMovies[j][1]))
-        Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3], "Poster": Poster})
+        #Poster = gp.getposter(tconst=str(TempMovies[j][0]), Name=str(TempMovies[j][1]))
+        #Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3], "Poster": Poster})
+        
+        Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3]})
+        """
 
     return Movies
 
 #____________________________________________________________________________________________________________________
 
-def GetFromTwoGenre(MainOutputList, Movies, Cursor, year, random):
+def GetFromTwoGenre(MainOutputList, Movies, Cursor, year, Range):
 
-    TempList = MainOutputList[:3]
+    TempList = MainOutputList
 
     for index1 in range(len(TempList)):
         Gen1 = TempList[index1]
 
         for index2 in range(index1+1, len(TempList)):
             Gen2 = TempList[index2]
-        
-            command = "Select tconst, primaryTitle, genres, startYear from data2 "
-            command = TemplateCommand('where', Gen1, ' ', command)
-            command = TemplateCommand('AND', Gen2, ';', command)
+            command = "Select tconst, primaryTitle, startYear, runtimeMinutes, genres, directors, averageRating, changeFactor, poster from details_data_movie where startYear > {0} ".format(year)
+            command = TemplateCommand('and ', Gen1, ' ', command)
+            command = TemplateCommand('and ', Gen2, ' ', command)
+            command += "and changeFactor<averageRating/100 ORDER BY changeFactor, averageRating DESC;"
             #print(command)
 
             Cursor.execute(command)
             Result = Cursor.fetchall()
+
+            """
+            max = 0
+            for j in Result:
+                if j[6]>max:
+                    max = j[6]
+                    value = j
+            """
+            """
+            max1 = 0
+            max2 = 9999999
+            value = []
+            for j in range(len(Result)):
+                if Result[j][6]>max1 and Result[j][7]<max2:
+                    max1 = Result[j][6]
+                    max2 = Result[j][7]
+                    value.append(j)
+            """
+            """
+            print("length - %d" %len(Result))
+            print("value - ")
+            print(value, end="\n\n")
+            """
+
+            for index in range(Range):
+                Movies.append(Result[index])
+            """
             TempMovies = []
     
             for j in range(len(Result)):
@@ -71,67 +124,80 @@ def GetFromTwoGenre(MainOutputList, Movies, Cursor, year, random):
             j = random.randint(0, len(TempMovies)-1)
             MovieIndex = len(Movies)
             href_movie = f"https://www.imdb.com/title/{TempMovies[j][0]}/"
-            Poster = gp.getposter(tconst=str(TempMovies[j][0]), Name=str(TempMovies[j][1]))
-            Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3], "Poster": Poster})
-
+            #Poster = gp.getposter(tconst=str(TempMovies[j][0]), Name=str(TempMovies[j][1]))
+            #Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3], "Poster": Poster})
+            
+            Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3]})
+            """
     
     return Movies
 
-#____________________________________________________________________________________________________________________
-
-def GetFromThreeGenre(MainOutputList, Movies, Cursor, year, random):
-
-    Gen1 = MainOutputList[0]
-    Gen2 = MainOutputList[1]
-    Gen3 = MainOutputList[2]
-
-    command = "Select tconst, primaryTitle, genres, startYear from data2 "
-    command = TemplateCommand('where', Gen1, ' ', command)
-    command = TemplateCommand('AND', Gen2, ' ', command)
-    command = TemplateCommand('AND', Gen3, ';', command)
-    #print(command)
-
-    Cursor.execute(command)
-    Result = Cursor.fetchall()
-    TempMovies = []
-    
-    for j in range(len(Result)):
-        if int(Result[j][3])>year and Result[j][3] != '\\N':
-            TempMovies.append(Result[j])
-
-    j = random.randint(0, len(TempMovies)-1)
-    MovieIndex = len(Movies)
-    href_movie = f"https://www.imdb.com/title/{TempMovies[j][0]}/"
-    Poster = gp.getposter(tconst=str(TempMovies[j][0]), Name=str(TempMovies[j][1]))
-    Movies.append({"tconst": TempMovies[j][0],"href_movie": href_movie, "Name": TempMovies[j][1], "Genres": TempMovies[j][2], "Year": TempMovies[j][3], "Poster": Poster})
-
-    return Movies
-    
 #____________________________________________________________________________________________________________________
 
 def getMovies(MainOutputList):
     
-    mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "22072000", database = "test")
-    Cursor = mydb.cursor()
+    try:
+        mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "22072000", database = "spotmovie")
+        Cursor = mydb.cursor(buffered=True)
+        
+        Movies = []
+        #Movies = GetFromOneGenre(MainOutputList, Movies, Cursor, 2000, 2)  #n2
+        Movies = GetFromTwoGenre(MainOutputList, Movies, Cursor, 1980, 5)  #n3
+        #Movies = GetFromThreeGenre(MainOutputList, Movies, Cursor, 2010, random) #n
+        
+        m = pd.DataFrame(Movies, columns=["tconst", "primaryTitle", "startYear", "runtimeMinutes", "genres", "directors", "averageRating", "changeFactor", "poster"])
+        m.drop_duplicates(subset ="tconst", keep = 'first', inplace = True)
+        m.sort_values("averageRating", ascending = False, inplace = True)
+        #file = open("RedirectedOutput.json", "w")
+        #JSONdata = json.dumps(Movies, ensure_ascii= False)
+
+        #file.write(JSONdata)
+        #file.close()
+
+        return m.iloc[:,:].values
     
-    Movies = []
+    except mysql.connector.Error as error:
+        print("Failed to get the data from the database - {}".format(error))
 
-    Movies = GetFromOneGenre(MainOutputList, Movies, Cursor, 2010, random)
-    Movies = GetFromTwoGenre(MainOutputList, Movies, Cursor, 2010, random)
-    #Movies = GetFromThreeGenre(MainOutputList, Movies, Cursor, 2010, random)
-    
-
-    #file = open("RedirectedOutput.json", "w")
-    JSONdata = json.dumps(Movies, ensure_ascii= False)
-
-    #file.write(JSONdata)
-    #file.close()
-
-    return JSONdata
+    finally:
+        if mydb.is_connected():
+            Cursor.close()
+            mydb.close()
+            print("MySQL connection is closed")
 
 #____________________________________________________________________________________________________________________
+"""
+MainOutputList = ['ACTION', 'SPORT', 'CRIME', 'ADVENTURE']
+#MainOutputList = ['ROMANCE', 'CRIME', 'HORROR']
 
-"""MainOutputList = ['ACTION', 'ROMANCE', 'SPORT', 'CRIME', 'ADVENTURE']
-
+St = time.time()
 Movies = getMovies(MainOutputList)
-print(Movies)"""
+Et = time.time()
+print("Time elapsed - {0}".format(Et-St))
+
+userRequest = True      #Assuming the user had Logged in
+try:  
+    start = 0
+    end = 10 
+    while(userRequest):
+        print(tabulate(Movies[start:end], tablefmt="pipe"))
+        print(len(Movies))
+
+        if end == len(Movies):
+            print("----------------------------------------THANKS FOR CHOOSING OUR RECOMMENDATIONS----------------------------------------")
+            break
+
+        i = int(input("LOAD MORE: (1 for yes/ 0 for exit) : "))
+        if i == 0:
+            userRequest = False
+            print("----------------------------------------THANKS FOR CHOOSING OUR RECOMMENDATIONS----------------------------------------")
+        else:   
+            start = end
+            end += 10
+            if(end>=len(Movies)):
+                end = len(Movies)
+                print("This is the last possible iteration for NOW. ")         
+     
+except ValueError:
+    print("Wrong Value")    
+"""
